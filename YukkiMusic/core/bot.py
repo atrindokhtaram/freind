5e98fce15f2a1765d7f4604bb0771751a90dc23d
atrindokhtaram <moha.face9@gmail.com>
2024-11-16 06:22:06 +0330

    Update bot.py

--- YukkiMusic/core/bot.py
+++ YukkiMusic/core/bot.py
@@ -24,7 +24,7 @@ class YukkiBot(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot")
         super().__init__(
-            "PyKalier",
+            "ATRmusic",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
